15a5e1054781485d458f9be13681a75393391737
mohonovproduction <mohonovproduction@yandex.ru>
2025-03-22 17:12:58 +0300

    zopa

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/ITMO_Note/' : '/',
+    publicPath: '/ITMO_Note/',
     chainWebpack: config => {
         config.module
             .rule('md')
