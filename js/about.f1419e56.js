a941ee7facdf39ea6df1c6a61dcde1101dacac73
MohonovProduction <MohonovProduction@users.noreply.github.com>
2025-03-22 16:55:14 +0000

    deploy: 721a01fb2fd0f18c2f7f695199d0f9a10bb12b3a

--- js/about.f1419e56.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkdesign_notes"]=self["webpackChunkdesign_notes"]||[]).push([[594],{709:function(n,e,s){s.r(e),s.d(e,{default:function(){return o}});var t=s(641);const u={class:"about"};function a(n,e){return(0,t.uX)(),(0,t.CE)("div",u,e[0]||(e[0]=[(0,t.Lk)("h1",null,"This is an about page",-1)]))}var r=s(262);const c={},i=(0,r.A)(c,[["render",a]]);var o=i}}]);
-//# sourceMappingURL=about.f1419e56.js.map
