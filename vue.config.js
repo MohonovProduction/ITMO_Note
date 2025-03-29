e6323f0c69bbc9826a4a0b2b1bbbcfc35fd43e00
mohonovproduction <mohonovproduction@yandex.ru>
2025-03-29 23:37:24 +0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,15 @@ module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/ITMO_Note/'
         : '/',
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://185.72.145.216:5000',
+                changeOrigin: true,
+                pathRewrite: { '^/api': '' }
+            }
+        }
+    },
     chainWebpack: config => {
         config.module
             .rule('md')
